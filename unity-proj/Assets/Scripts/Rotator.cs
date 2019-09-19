5a07883083912d8f85e8075a360c4d24cc0c43fc
Ignacio Pascual <vonflaken@gmail.com>
2019-09-19 14:31:21 +0200

    Added behaviour to windmill traps, they rotate on site.

---
+++ unity-proj/Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target = null;
+    [SerializeField, Tooltip("In degrees per second")]
+    private float _speed = 45f;
+    [SerializeField]
+    private Vector3 _axis = Vector3.up;
+
+    private void Update()
+    {
+        float deltaSpeed = _speed * Time.deltaTime;
+        transform.rotation *= Quaternion.Euler(deltaSpeed * _axis.x, deltaSpeed * _axis.y, deltaSpeed * _axis.z);
+    }
+}
